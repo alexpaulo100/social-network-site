f5d1c0e78d941cd4ae98cd503dafa652ff72a074
Alex Silva <alex.paulo100@gmail.com>
2021-03-21 19:40:45 -0300

    followers and following

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "home#index"
     resources :admins
-    resources :user, only: :index
+    resources :users, only: :index
     resources :posts, only: [:index, :show]
   end
 
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
         name { "Alex" }
-        email { "user@user.com" }
+        email { "user#{rand(1...1000)}@user.com" }
         phone { "999991515"}
         avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png') }
         password {"123456"}
