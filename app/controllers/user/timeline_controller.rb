cc0b0e7f0b0f06a1e6346a542691f34b3b163127
Alex Silva <alex.paulo100@gmail.com>
2021-03-22 19:26:11 -0300

    timeline and user details

--- app/controllers/user/timeline_controller.rb
+++ app/controllers/user/timeline_controller.rb
@@ -1,6 +1,8 @@
 class User::TimelineController < UserController
 
-def index
-end
+    def index
+        @posts = Post.where(user_id: current_user.following.pluck(:id)).order(created_at: :desc)
+
+    end
 
 end
